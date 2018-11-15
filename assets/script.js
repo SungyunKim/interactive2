ec7ae7f1730111af6408b3c80938aaf3ddd38cd2
SungyunKim <ksy056605@gmail.com>
2018-11-15 14:37:54 -0800

    asdfasdf

---
+++ assets/script.js
@@ -0,0 +1,218 @@
+$(document).ready(function(){
+
+    // add a zero to times below 10
+    function addZero(i) {
+        if (i < 10) {
+            i = "0" + i;
+        }
+        return i;
+    }
+
+    // set up time getting function
+    function getTime(){
+        // get the date
+        var d = new Date();
+        
+        // get hours, minutes, and seconds
+        var h = addZero(d.getHours());
+        var m = addZero(d.getMinutes());
+        var s = addZero(d.getSeconds());
+        
+        // update the clock's text
+        $('#clock').html(h + ":" + m + ":" + s);
+
+        // rotate clock based on the minute
+        // $('#clock').css('transform', 'rotate('+ s * 6 +'deg');
+
+        // detect a specific time
+        // if(m == 00){
+        //  $('body').addClass('noon');
+        // } else {
+        //  $('body').removeClass('noon');
+        // }
+    }
+
+    // call getTime every 1 second
+    setInterval(function() {
+      getTime();
+    }, 1000);
+
+
+});
+
+$(document).ready(function(){
+
+    // add a zero to times below 10
+    function addZero(i) {
+        if (i < 10) {
+            i = "0" + i;
+        }
+        return i;
+    }
+
+    // set up time getting function
+    function getTime(){
+        // get the date
+        var d = new Date();
+        
+        // get hours, minutes, and seconds
+        var h = addZero(d.getHours()+2);
+        var m = addZero(d.getMinutes());
+        var s = addZero(d.getSeconds());
+        
+        // update the clock's text
+        $('#clock2').html(h + ":" + m + ":" + s);
+
+        // rotate clock based on the minute
+        // $('#clock').css('transform', 'rotate('+ s * 6 +'deg');
+
+        // detect a specific time
+        // if(m == 00){
+        //  $('body').addClass('noon');
+        // } else {
+        //  $('body').removeClass('noon');
+        // }
+    }
+
+    // call getTime every 1 second
+    setInterval(function() {
+      getTime();
+    }, 1000);
+
+
+});
+
+$(document).ready(function(){
+
+    // add a zero to times below 10
+    function addZero(i) {
+        if (i < 10) {
+            i = "0" + i;
+        }
+        return i;
+    }
+
+    // set up time getting function
+    function getTime(){
+        // get the date
+        var d = new Date();
+        
+        // get hours, minutes, and seconds
+        var h = addZero(d.getHours()+4);
+        var m = addZero(d.getMinutes());
+        var s = addZero(d.getSeconds());
+        
+        // update the clock's text
+        $('#clock3').html(h + ":" + m + ":" + s);
+
+        // rotate clock based on the minute
+        // $('#clock').css('transform', 'rotate('+ s * 6 +'deg');
+
+        // detect a specific time
+        // if(m == 00){
+        //  $('body').addClass('noon');
+        // } else {
+        //  $('body').removeClass('noon');
+        // }
+    }
+
+    // call getTime every 1 second
+    setInterval(function() {
+      getTime();
+    }, 1000);
+
+
+});
+
+$(document).ready(function(){
+
+    // add a zero to times below 10
+    function addZero(i) {
+        if (i < 10) {
+            i = "0" + i;
+        }
+        return i;
+    }
+
+    // set up time getting function
+    function getTime(){
+        // get the date
+        var d = new Date();
+        
+        // get hours, minutes, and seconds
+        var h = addZero(d.getHours()-8);
+        var m = addZero(d.getMinutes());
+        var s = addZero(d.getSeconds());
+        
+        // update the clock's text
+        $('#clock4').html(h + ":" + m + ":" + s);
+
+        // rotate clock based on the minute
+        // $('#clock').css('transform', 'rotate('+ s * 6 +'deg');
+
+        // detect a specific time
+        // if(m == 00){
+        //  $('body').addClass('noon');
+        // } else {
+        //  $('body').removeClass('noon');
+        // }
+    }
+
+    // call getTime every 1 second
+    setInterval(function() {
+      getTime();
+    }, 1000);
+
+
+});
+
+$(document).ready(function(){
+
+    // add a zero to times below 10
+    function addZero(i) {
+        if (i < 10) {
+            i = "0" + i;
+        }
+        return i;
+    }
+
+    // set up time getting function
+    function getTime(){
+        // get the date
+        var d = new Date();
+        
+        // get hours, minutes, and seconds
+        var h = addZero(d.getHours()-5);
+        var m = addZero(d.getMinutes());
+        var s = addZero(d.getSeconds());
+        
+        // update the clock's text
+        $('#clock5').html(h + ":" + m + ":" + s);
+
+        // rotate clock based on the minute
+        // $('#clock').css('transform', 'rotate('+ s * 6 +'deg');
+
+        // detect a specific time
+        // if(m == 00){
+        //  $('body').addClass('noon');
+        // } else {
+        //  $('body').removeClass('noon');
+        // }
+    }
+
+    // call getTime every 1 second
+    setInterval(function() {
+      getTime();
+    }, 1000);
+
+    function myFunction() {
+    document.getElementById(".clock2").style.WebkitPerspective = "100px"; // Chrome, Safari and Opera
+    document.getElementById(".clock2").style.perspective = "100px";
+}
+
+
+});
+
+
+
+
