bb4c941d300a43a19ae3b1093515823d1e5d1e8f
SungyunKim <ksy056605@gmail.com>
2018-11-30 02:19:23 -0800

    ㅁㄴㅇㄹ

--- assets/js/Onlineexhibition.js
+++ assets/js/Onlineexhibition.js
@@ -1,7 +1,35 @@
 $(document).ready(function(){
     $('.pic1pic').click(function() {
 		$('.pic1d1').toggleClass('show');
+		document.getElementById("descrit2").innerHTML = "";
 	});
 });
 
+$(document).ready(function(){
+    $('.pic1pic1').click(function() {
+		$('.pic1d2').toggleClass('show');
+		document.getElementById("descrit3").innerHTML = "";
+	});
+});
+
+$(document).ready(function(){
+    $('.pic1pic2').click(function() {
+		$('.pic1d3').toggleClass('show');
+		document.getElementById("descrit4").innerHTML = "";
+	});
+});
+
+$(document).ready(function(){
+    $('.pic1pic3').click(function() {
+		$('.pic1d4').toggleClass('show');
+		document.getElementById("descrit5").innerHTML = "";
+	});
+});
+
+$(document).ready(function(){
+    $('.pic1pic4').click(function() {
+		$('.pic1d5').toggleClass('show');
+		document.getElementById("descrit5").innerHTML = "";
+	});
+});
 
